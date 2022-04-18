d684ef996669c42e3b78ad2a9210b05f2959d703
Pineapplentt <joao.santana@sptech.school>
2022-04-18 18:02:02 -0300

    commit 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Atividade-PI-aqcuatecCPF
+# API Projeto Site - 1º Semestre
+Esta API será utilizada para conectar seu Website do Projeto de PI ao Banco de Dados.
---
+++ src/database/database local acquatec.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE acquatec;
+
+USE acquatec;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	cpf char(11)
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+    descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+); 
+
+CREATE TABLE medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	temperatura DECIMAL,
+	umidade DECIMAL,
+	momento DATETIME,
+	fk_aquario INT
+);
